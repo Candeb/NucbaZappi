8e8c7702cda77dd6fb80e2ca0913b6afb686b069
Fede <fedepolicano98@gmail.com>
2022-10-21 22:30:38 -0300

    commit full html and css

---
+++ data.js
@@ -0,0 +1,14 @@
+// Array de productos
+
+const products = [
+    {
+        id: 1,
+        nombre: "La Mr. Pit",
+        descripcion: "Solo para expertos",
+        image: "assets/Photo Menu.png",
+        categoria: "pizza",
+        isPopular: "yes",
+        precio: 350,
+    }, 
+];
+
